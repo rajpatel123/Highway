e312cefff929c68975ae7490bffd71c1d1cf1e83
sarthaksharma <sarthak3694@yahoo.in>
2019-01-09 18:02:14 +0530

    changes made on 9th Jan 2019.

--- app/src/main/java/com/videoapp/fagments/RecommendedFragment.java
+++ app/src/main/java/com/videoapp/fagments/RecommendedFragment.java
@@ -58,21 +58,6 @@ public class RecommendedFragment extends Fragment {
     RecyclerView rvRecommended;
     SharedPreferences preferences;
 
-
-    List<String> arrProfilePics = new ArrayList<>(Arrays.asList("https://www.sheffield.ac.uk/polopoly_fs/1.739455!/image/A_Cochrane_300x300.jpg",
-            "https://andersonstrathern.co.uk/uploads/images/ws_cropper/4_0x0_439x439_440x440_bruce_farquhar.jpg",
-            "https://www.victoria.ac.nz/images/staffpics/jason-young.jpg",
-            "https://st.depositphotos.com/1008939/1880/i/450/depositphotos_18807295-stock-photo-portrait-of-handsome-man.jpg",
-            "https://warwick.ac.uk/fac/soc/economics/staff/aadvani/profile.jpg",
-            "https://files.list.co.uk/images/2018/03/23/295e8e8950efadea7ae9acbe51572667b7b43270-original-LST284579.jpg",
-            "https://st.depositphotos.com/1008939/1880/i/450/depositphotos_18807295-stock-photo-portrait-of-handsome-man.jpg",
-            "https://warwick.ac.uk/fac/soc/economics/staff/aadvani/profile.jpg"));
-
-    List<String> arrNames = new ArrayList<>(Arrays.asList("John Marley", "Steven Grace", "Charlie Harper", "Alan Harper", "Jeff Strongman",
-            "Richard Gracio", "Jacob Muff", "Andrew Phill"));
-
-    List<String> arrDesc = new ArrayList<>(Arrays.asList("hello", "hi", "", "", "make friends", "hello again", "remarkable", "thats it"));
-
     public RecommendedFragment() {
         // Required empty public constructor
     }
