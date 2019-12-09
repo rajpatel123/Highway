4210799336fcf6af870bcefca093960ee39b8713
raj <raj.bahadur@ziploan.biz>
2019-12-09 09:41:22 +0530

    Merge branch 'master' of https://github.com/rajpatel123/Highway
    # Conflicts:
    #	app/src/main/AndroidManifest.xml

--- app/src/main/java/com/highway/customer/customerActivity/GoodTypeDetailActivity.java
+++ app/src/main/java/com/highway/customer/customerActivity/GoodTypeDetailActivity.java
@@ -3,6 +3,7 @@ package com.highway.customer.customerActivity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.highway.R;
 
@@ -12,5 +13,20 @@ public class GoodTypeDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_good_type_detail);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        getSupportActionBar().setTitle("Select your goods type");
+
     }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        onBackPressed();
+        return super.onOptionsItemSelected(item);
+    }
+
 }
---
+++ app/src/main/java/com/highway/customer/customerActivity/WebViewActivity.java
@@ -0,0 +1,162 @@
+package com.highway.customer.customerActivity;
+
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.highway.R;
+import com.highway.utils.Utils;
+
+import static android.view.View.GONE;
+
+
+public class WebViewActivity extends AppCompatActivity {
+    private static final String TAG = "WebViewActivity";
+
+    ProgressBar progressBar;
+    WebView mWebView;
+    RelativeLayout rlNetworkUI;
+    RelativeLayout rlWebView;
+
+    String title;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview_layout);
+
+        mWebView = findViewById(R.id.dataWebView);
+        progressBar = findViewById(R.id.progressBar);
+        rlNetworkUI = findViewById(R.id.rlNetworkUI);
+        rlWebView = findViewById(R.id.rlWebView);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+        }
+        title = getIntent().getStringExtra("title");
+        getSupportActionBar().setTitle("Terms & Conditions");
+        try {
+            initComponent();
+        } catch (Exception e) {
+            e.printStackTrace();
+            String additionalDetail = null;
+        }
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        onBackPressed();
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void initComponent() throws Exception {
+
+        mWebView.setWebViewClient(new myWebClient());
+        mWebView.getSettings().setJavaScriptEnabled(true);
+
+
+        getSupportActionBar().setTitle(title);
+        if (!Utils.isInternetConnected(this)) {
+            rlWebView.setVisibility(GONE);
+            rlNetworkUI.setVisibility(View.VISIBLE);
+//            rlNetworkUI.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    try {
+//                        initComponent();
+//                    } catch (Exception e) {
+//                        e.printStackTrace();
+//                    }
+//                }
+//            });
+            return;
+        }
+
+        rlWebView.setVisibility(View.VISIBLE);
+        rlNetworkUI.setVisibility(View.GONE);
+
+        progressBar.setVisibility(View.VISIBLE);
+        mWebView.loadUrl("http://13.234.161.7/termsncondition.php");
+//        switch (title) {
+//            case "Terms & Conditions":
+//                progressBar.setVisibility(View.VISIBLE);
+//                mWebView.loadUrl("http://13.234.161.7/termsncondition.php");
+//                break;
+//
+//            case "Privacy Policy":
+//                progressBar.setVisibility(View.VISIBLE);
+//                mWebView.loadUrl("http://13.234.161.7/privacy-policy.php");
+//                break;
+//
+//            case "Read More":
+//                progressBar.setVisibility(View.VISIBLE);
+//                mWebView.loadUrl("http://www.readingrockets.org/article/reading-information");
+//                break;
+//            case "condition":
+//                progressBar.setVisibility(View.VISIBLE);
+//                mWebView.loadUrl("http://reddyenterprise.com/education/termsncondition.php");
+//                break;
+//
+//            case "FAQ":
+//                progressBar.setVisibility(View.VISIBLE);
+//                mWebView.loadUrl("http://13.234.161.7/dnafaq.php");
+//                break;
+//            case "Report":
+//                progressBar.setVisibility(View.VISIBLE);
+//                mWebView.loadUrl("http://reddyenterprise.com/education/termsncondition.php");
+//                break;
+//
+//            case "About Us":
+//                progressBar.setVisibility(View.VISIBLE);
+//                mWebView.loadUrl("http://reddyenterprise.com/education/termsncondition.php");
+//                break;
+//
+//
+//            case "Contact Us":
+//                progressBar.setVisibility(View.VISIBLE);
+//                mWebView.loadUrl("http://reddyenterprise.com/education/termsncondition.php");
+//                break;
+//
+//        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    public class myWebClient extends WebViewClient {
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            progressBar.setVisibility(View.VISIBLE);
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            progressBar.setVisibility(GONE);
+        }
+    }
+}
+
